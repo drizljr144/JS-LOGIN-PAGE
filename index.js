743a0a925f291896df87ed5c798226799a19a182
SAMUEL CHIDERA AZUH <samuelazuh14@gmail.com>
2026-09-15 16:11:53 +0100

    The relocation of files

---
+++ index.js
@@ -0,0 +1,21 @@
+let Fname = document.getElementById("Fname").value;
+let Lname = document.getElementById("Lname").value;
+let Passoword = document.getElementById("Passoword").value;
+let ConfirmPassword = document.getElementById("ConfirmPassword").value;
+let TargetUrl = "www.google.com"
+Verified = false;
+
+let User1 = {
+    FirstName: 'Samuel',
+    LastName : 'Azuh',
+    Password : '#2507DRiz_26!'
+};
+
+    if (Passoword === ConfirmPassword){
+       
+    }
+    else{}
+
+    function VerifyPassword (){
+                Verified = true;
+}
