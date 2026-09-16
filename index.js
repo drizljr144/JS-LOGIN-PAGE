637ea4be30b0e4fe78e133a4da4480fa933b8028
SAMUEL CHIDERA AZUH <samuelazuh14@gmail.com>
2026-09-17 00:18:33 +0100

    I made the buttons work

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-let Fname = document.getElementById("Fname").value;
-let Lname = document.getElementById("Lname").value;
-let Passoword = document.getElementById("Passoword").value;
-let ConfirmPassword = document.getElementById("ConfirmPassword").value;
-let TargetUrl = "www.google.com"
-Verified = false;
+ function RedirectFacebook() {
+      let Fname = document.getElementById("Fname").value;
+      let Lname = document.getElementById("Lname").value;
+      let Password = document.getElementById("Password").value;
+      let ConfirmPassword = document.getElementById("ConfirmPassword").value;
 
-let User1 = {
-    FirstName: 'Samuel',
-    LastName : 'Azuh',
-    Password : '#2507DRiz_26!'
-};
+      let Verified = false;
 
-    if (Passoword === ConfirmPassword){
-       
+      let User1 = {
+        FirstName: 'Samuel',
+        LastName: 'Azuh',
+        Password: '123456'
+      };
+
+      if (Fname === User1.FirstName && Lname === User1.LastName && Password === User1.Password && ConfirmPassword === User1.Password) {
+        Verified = true;
+      } else {
+        Verified = false;
+      }
+
+      let TargetUrl = "signup.html";
+      let FailedUrl = "https://www.bing.com";
+
+      if (Verified === true) {
+        window.location.href = TargetUrl;
+      } else {
+        window.location.href = FailedUrl;
+      }
     }
-    else{}
-
-    function VerifyPassword (){
-                Verified = true;
-}
